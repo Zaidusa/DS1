b4a01644f6cde806149b368696ca4da86510c2af
venu <vellampalliwrites@gmail.com>
2022-12-13 10:39:41 +0530

    added path1

--- DSP/Bank.py
+++ DSP/Bank.py
@@ -3,4 +3,9 @@ import streamlit as st
 import os
 ROOT_DIR = os.path.abspath(os.curdir)
 
-print(ROOT_DIR)
+print("root is",ROOT_DIR)
+
+video_file = open('Bank.mp4', 'rb')
+video_bytes = video_file.read()
+
+st.video(video_bytes)
