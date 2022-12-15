74b218d1f5bffaf9650efcf3fbc363427d67390a
venu <vellampalliwrites@gmail.com>
2022-12-15 11:22:57 +0530

    added sandy

--- DSP/Bank.py
+++ DSP/Bank.py
@@ -1,9 +1,9 @@
-# import the streamlit library
 import streamlit as st
 from PIL import Image
 
 image = Image.open('TSPSC.png')
-st.image(image, caption='Sunrise by the mountains')
+st.image(image, caption='TSPSC Results')
+
 st.header("TSPSC Group 1 Results")
 st.subheader("TSPSC Group 1 Results soon in december")
 st.text("TSPSC RESULTS WILL BE SOON PUBLISHED in below Link {}".format("https://www.tspsc.gov.in/"))
---
+++ DSP/sandy.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header("my first web site")
+st.subheader("am learning python")
+st.radio("gender",("male","female"))
+st.selectbox("age",[10,20,30])
+st.button("submit")
